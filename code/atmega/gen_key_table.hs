502be640c4a8b2239e7b33958bcda2b27f0881d9
iliis <samuelbryner@gmx.ch>
2014-05-11 01:06:36 +0200

    Cleanup and use keycode list.

--- code/atmega/gen_key_table.hs
+++ code/atmega/gen_key_table.hs
@@ -2,9 +2,9 @@ import Data.Char
 import Text.Printf
 
 data KeyEntry =
-	KeyKeyboard { modifier :: String, keycode :: String } |
-	KeyMultimedia { keycode :: String } |
-	KeyNone deriving (Show) 
+  KeyKeyboard { modifier :: String, keycode :: String } |
+  KeyMultimedia { keycode :: String } |
+  KeyNone deriving (Show) 
 
 page KeyNone           = 0
 page (KeyKeyboard _ _) = 1
@@ -18,45 +18,54 @@ code_key KeyNone = "0 /* unused */"
 code_key (KeyKeyboard _ k) = "KEY_" ++ k
 code_key (KeyMultimedia k) = "MULT_" ++ k
 
-fromChar c = KeyKeyboard "0" ("KEY_" ++ c)
+fromChar c = KeyKeyboard "0" c
 
 keyline n entry = "/* " ++  (printf "%3d" n) ++ " */   {" ++ (show (page entry)) ++ ", " ++ (code_mod entry) ++ ", " ++ (code_key entry) ++ "}"
 
 
-
--- actual generation (change stuff here if you want a different list) --
+-- list of keys (add more here)
 
 readable_keys = ['A'..'Z'] ++ ['0'..'9']
 non_readable_keys = [(ord '\b', "BACKSPACE"),
-					 (ord '\n', "ENTER"),
-					 (27, "ESCAPE"),
-					 (ord '+', "PLUS"),
-					 (ord '-', "MINUS"),
-					 (ord ' ', "SPACE")]
+           (ord '\n', "RETURN"),
+           (27, "ESCAPE"),
+           (ord '+', "PLUS"),
+           (ord '-', "MINUS"),
+           (ord ' ', "SPACE")]
 
 non_ascii_keys = 
-	[ KeyKeyboard "0" "PAUSE"
-	, KeyKeyboard "0" "PAGE_UP"
-	, KeyKeyboard "0" "PAGE_DOWn"
-	, KeyKeyboard "0" "CUR_RIGHT"
-	, KeyKeyboard "0" "CUR_LEFT"
-	, KeyKeyboard "0" "CUR_DOWN"
-	, KeyKeyboard "0" "CUR_UP"
-	, KeyKeyboard "0" "STOP"
-	, KeyMultimedia "MUTE" ]
-	++ (map (\ i -> KeyKeyboard "0" ("F" ++ (show i))) [1..12])
+  [ KeyKeyboard "0" "PAUSE"
+  , KeyKeyboard "0" "PAGE_UP"
+  , KeyKeyboard "0" "PAGE_DOWN"
+  , KeyKeyboard "0" "CUR_RIGHT"
+  , KeyKeyboard "0" "CUR_LEFT"
+  , KeyKeyboard "0" "CUR_DOWN"
+  , KeyKeyboard "0" "CUR_UP"
+  , KeyKeyboard "0" "STOP"
+  , KeyMultimedia "MUTE"
+  , KeyMultimedia "VOL_UP"
+  , KeyMultimedia "VOL_DOWN" ]
+  ++ (map (\ i -> KeyKeyboard "0" ("F" ++ (show i))) [1..12])
 
--- now sort these according to their ASCII code (if any, otherwise just insert it at an unused ASCII code)
+-- actual generation (change stuff here if you want a differently organized list)
+-- sort these according to their ASCII code (if any, otherwise just insert it at an unused ASCII code)
 
 ascii_keys = non_readable_keys ++ (map (\ c -> (ord c, [c])) readable_keys)
 
 num_keys = max ((length ascii_keys) + (length non_ascii_keys)) (maximum (map fst ascii_keys))
 
 keylines i non_ascii = case (lookup i ascii_keys) of
-		Just k  -> [ keyline i (fromChar k) ]     ++ (keylines (i+1) non_ascii)
-		Nothing -> case non_ascii of
-					[] -> if i > maximum (map fst ascii_keys) then [] else [ keyline i KeyNone ] ++ keylines (i+1) []
-					_  -> [ keyline i (head non_ascii) ] ++ (keylines (i+1) (tail non_ascii))
+    Just k  -> [ keyline i (fromChar k) ]     ++ (keylines (i+1) non_ascii)
+    Nothing ->
+      if null non_ascii
+        then
+          if i > maximum (map fst ascii_keys)
+            then
+              [] -- end recursion (no more ascii or non-ascii keys)
+            else
+              [ keyline i KeyNone ] ++ keylines (i+1) [] -- there are still ascii keys in the list
+        else
+          [ keyline i (head non_ascii) ] ++ (keylines (i+1) (tail non_ascii))
 
 main = do
-	putStrLn (foldl1 (\ l r -> l ++ ",\n" ++ r) (keylines 1 non_ascii_keys))
+  putStrLn (foldl1 (\ l r -> l ++ ",\n" ++ r) (keylines 1 non_ascii_keys))
